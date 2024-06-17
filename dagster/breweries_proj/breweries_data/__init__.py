02c787b536ef9d848c367429d89e0c8806bb60e7
Arthur Ribeiro <arthuroribeiro@hotmail.com>
2024-06-16 23:06:07 -0300

    Adicionando job + schedule

--- dagster/breweries_proj/breweries_data/__init__.py
+++ dagster/breweries_proj/breweries_data/__init__.py
@@ -4,12 +4,27 @@ from .assets import (
     gold_dim_breweries,
 )
 from .io_managers import DeltaLakeIOManager, TextIOManager
-from dagster import Definitions
+from dagster import Definitions, define_asset_job, AssetKey, ScheduleDefinition
 import os
+
+# Job
+breweries_job = define_asset_job(
+    name="breweries_job",
+    selection=[
+        AssetKey(["bronze", "raw_breweries"]),
+        AssetKey(["silver", "dim_breweries"]),
+        AssetKey(["gold", "vw_breweries_agg"]),
+    ],
+)
+
+# Schedule
+breweries_schedule = ScheduleDefinition(job=breweries_job, cron_schedule="0 10 * * *")
 
 # Definitions
 def_root_uri = os.environ["STORAGE_PATH"]
 defs = Definitions(
+    jobs=[breweries_job],
+    schedules=[breweries_schedule],
     assets=[bronze_raw_breweries, silver_dim_breweries, gold_dim_breweries],
     resources={
         "io_manager_delta": DeltaLakeIOManager(root_uri=def_root_uri),
